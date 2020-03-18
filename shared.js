3bbeca0d047bbbb2d3ed027a154a0002a0e2c378
gvillalb <gianluca.nadir.villalba@capgemini.com>
2020-03-18 12:07:27 +0100

    opening and closing the hamburger menu

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop')
 var modal = document.querySelector('.modal')
 var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButtons = document.querySelectorAll('.plan button')
+var toggleButton = document.querySelector('.toggle-button')
+var mobileNav = document.querySelector('.mobile-nav')
 
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
@@ -11,7 +13,17 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     })
 }
 
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none'
+    closeModal()
+})
+
 modalNoButton.addEventListener('click', closeModal)
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block'
+    backdrop.style.display = 'block'
+})
 
 function closeModal() {
     modal.style.display = 'none'
