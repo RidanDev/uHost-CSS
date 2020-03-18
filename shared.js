b1b46b3f91ff36f8f5c8745bfab2e18b2cf0aa95
gvillalb <gianluca.nadir.villalba@capgemini.com>
2020-03-18 12:50:48 +0100

    manipulating element classes

--- shared.js
+++ shared.js
@@ -8,24 +8,32 @@ var mobileNav = document.querySelector('.mobile-nav')
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
-        modal.style.display = 'block'
-        backdrop.style.display = 'block'
+        //modal.style.display = 'block'
+        //backdrop.style.display = 'block'
+        //modal.className = 'open' //this will actually overwrite the complete class
+        modal.classList.add('open') //thi will add the class
+        backdrop.classList.add('open')
     })
 }
 
 backdrop.addEventListener('click', () => {
-    mobileNav.style.display = 'none'
+    //mobileNav.style.display = 'none'
+    mobileNav.classList.remove('open')
     closeModal()
 })
 
 modalNoButton.addEventListener('click', closeModal)
 
 toggleButton.addEventListener('click', () => {
-    mobileNav.style.display = 'block'
-    backdrop.style.display = 'block'
+    //mobileNav.style.display = 'block'
+    //backdrop.style.display = 'block'
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
 })
 
 function closeModal() {
-    modal.style.display = 'none'
-    backdrop.style.display = 'none'
+    //modal.style.display = 'none'
+    //backdrop.style.display = 'none'
+    modal.classList.remove('open')
+    backdrop.classList.remove('open')
 }
